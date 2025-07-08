0751fc4281f0d870f05ff5c8cce2957e97e61e13
Debang chowdhury <sambhuchowdhury512@gmail.com>
2025-07-08 15:11:10 +0530

    commit

--- test1.java
+++ test1.java
@@ -1,5 +1,34 @@
+
+
 public class test1 {
+    public static int reverse(int arr[] ){
+
+        for(int i = arr.length-1; i >= 0; i--){
+            int first = i;
+            int last = arr.length-1-i;
+
+                if(first <= last){
+                    break;
+
+                }
+            swap(first , last , arr);
+            }
+
+
+        return -1;
+    }
+    public static void swap(int last , int first , int arr[]){
+        int temp = arr[first];
+        arr[first] = arr[last];
+        arr[last] = temp;
+    }
     public  static void main(String[] args){
-        System.out.println("Hello world");
+        int arr[] = {1,2,3,4,5,6,7,8,9,10};
+        int result = reverse(arr);
+        for(int i =0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+        System.out.println(result);
+
     }
 }
--- test3.java
+++ test3.java
@@ -1,2 +1,33 @@
 public class test3 {
+    public static void swapvaluescheck(int arr[] ,int swap ){
+        boolean found = false;
+        for(int i=0; i< arr.length;i++){
+            if(arr[i] == swap){
+               found = true;
+               break;
+            }
+        }
+        if(found){
+            System.out.println("It can be Swapped");
+        }
+        else{
+            System.out.println("It cannot be  swaped");
+        }
+
+    }
+    public static void swaping(int arr[] , int last , int first ){
+        int temp = arr[first];
+        arr[first] = arr[last];
+        arr[last] = temp;
+    }
+    public static void main(String[] args){
+        int arr[] = {1,2,3,4,5,6,7,8,9,10};
+        swapvaluescheck(arr , 8);
+        swapvaluescheck(arr , 2);
+        swaping(arr , 0 ,9);
+        for(int i =0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+
+    }
 }
