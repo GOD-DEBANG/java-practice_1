d68f25f356e6e87a8e736b5e65e9f534a53e4e6e
Debang chowdhury <sambhuchowdhury512@gmail.com>
2025-07-08 15:11:10 +0530

    commit

--- test2.java
+++ test2.java
@@ -1,3 +1,15 @@
 public class test2 {
-    
+    public static int sum(int arr[]){
+        int sum = 0;
+        for(int i = 0; i<arr.length; i++){
+
+            sum = sum+arr[i];
+        }
+        return sum;
+    }
+    public static void main(String[] args){
+        int arr[] = {1,2,3,4,5,6,7,8,9,10};
+        int result = sum(arr);
+        System.out.println(result);
+    }
 }
