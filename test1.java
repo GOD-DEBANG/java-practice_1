b91fb61ace38f0c349efcb1ae6eedb4d44f0a582
Debang chowdhury <sambhuchowdhury512@gmail.com>
2025-07-08 10:13:57 +0530

    commit

--- test1.java
+++ test1.java
@@ -1,11 +1,5 @@
 public class test1 {
-    public static int sum(int arr[]){
-        for(int i=0; i<arr.length; i++){
-            for(int j = 0; j<i; j++){
-                int sum = 0;
-                sum = sum + i;
-            }
-        }
-        return i;
+    public static void main(String[] args){
+        System.out.println("Hello world");
     }
 }
